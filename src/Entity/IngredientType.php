18c2da00afd9a5916a4380d7c88a3cd0e4b3bd15
raphael.durand1@etudiant.univ-reims.fr <raphael.durand1@univ-reims.fr>
2023-12-05 23:50:04 +0100

    Ajout de l'entité IngredientType

---
+++ src/Entity/IngredientType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientTypeRepository::class)]
+class IngredientType
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $ingredientTpName = null;
+
+    #[ORM\ManyToMany(targetEntity: ingredient::class, inversedBy: 'ingredientTypes')]
+    private Collection $ingredients;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIngredientTpName(): ?string
+    {
+        return $this->ingredientTpName;
+    }
+
+    public function setIngredientTpName(string $ingredientTpName): static
+    {
+        $this->ingredientTpName = $ingredientTpName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ingredient>
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(ingredient $ingredient): static
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients->add($ingredient);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(ingredient $ingredient): static
+    {
+        $this->ingredients->removeElement($ingredient);
+
+        return $this;
+    }
+}
