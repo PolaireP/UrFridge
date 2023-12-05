4ca7b83d273219e1db2f9b00e9f8787190b53af4
raphael.durand1@etudiant.univ-reims.fr <raphael.durand1@univ-reims.fr>
2023-12-05 23:50:19 +0100

    Ajout des repository

---
+++ src/Repository/RecipePhotoRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\RecipePhoto;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<RecipePhoto>
+ *
+ * @method RecipePhoto|null find($id, $lockMode = null, $lockVersion = null)
+ * @method RecipePhoto|null findOneBy(array $criteria, array $orderBy = null)
+ * @method RecipePhoto[]    findAll()
+ * @method RecipePhoto[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RecipePhotoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, RecipePhoto::class);
+    }
+
+//    /**
+//     * @return RecipePhoto[] Returns an array of RecipePhoto objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?RecipePhoto
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
