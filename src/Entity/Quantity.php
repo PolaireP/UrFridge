eabfc6c2f52846ef6fa45656ef95fe0235afd05a
raphael.durand1@etudiant.univ-reims.fr <raphael.durand1@univ-reims.fr>
2023-12-05 23:49:51 +0100

    Ajout de l'entité Quantity

---
+++ src/Entity/Quantity.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuantityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuantityRepository::class)]
+class Quantity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $quantity = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quantities')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?inventory $inventory = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quantities')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ingredient $ingredient = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?float
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?float $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getInventory(): ?inventory
+    {
+        return $this->inventory;
+    }
+
+    public function setInventory(?inventory $inventory): static
+    {
+        $this->inventory = $inventory;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?ingredient
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(?ingredient $ingredient): static
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+}
