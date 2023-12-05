4e5cba53c384d61167d059c93bca37ba237612ef
raphael.durand1@etudiant.univ-reims.fr <raphael.durand1@univ-reims.fr>
2023-12-05 23:49:04 +0100

    Ajout de l'entité Category

---
+++ src/Entity/Category.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $categoryName = null;
+
+    #[ORM\Column(length: 400)]
+    private ?string $categoryDescription = null;
+
+    #[ORM\ManyToMany(targetEntity: recipe::class, inversedBy: 'categories')]
+    private Collection $recipes;
+
+    public function __construct()
+    {
+        $this->recipes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryName(): ?string
+    {
+        return $this->categoryName;
+    }
+
+    public function setCategoryName(string $categoryName): static
+    {
+        $this->categoryName = $categoryName;
+
+        return $this;
+    }
+
+    public function getCategoryDescription(): ?string
+    {
+        return $this->categoryDescription;
+    }
+
+    public function setCategoryDescription(string $categoryDescription): static
+    {
+        $this->categoryDescription = $categoryDescription;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, recipe>
+     */
+    public function getRecipes(): Collection
+    {
+        return $this->recipes;
+    }
+
+    public function addRecipe(recipe $recipe): static
+    {
+        if (!$this->recipes->contains($recipe)) {
+            $this->recipes->add($recipe);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipe(recipe $recipe): static
+    {
+        $this->recipes->removeElement($recipe);
+
+        return $this;
+    }
+}
