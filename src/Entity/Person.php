2d07632edfadced964570f1f04f7c4ef76c555d2
dura0074 <raphael.durand1@etudiant.univ-reims.fr>
2023-12-06 16:56:04 +0100

    Correction nom table User -> Person

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -2,15 +2,15 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Repository\PersonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Entity(repositoryClass: PersonRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class Person
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
