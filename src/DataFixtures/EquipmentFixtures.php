0c6f724ef036dc8cc1f33997e0b2a9f829bfc86c
Alexandre Philippe <alexandre.philippe@etudiant.univ-reims.fr>
2023-12-09 18:41:11 +0100

    Implémentation du générateur de données d'équipements

--- src/DataFixtures/EquipmentFixtures.php
+++ src/DataFixtures/EquipmentFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EquipmentFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,9 +11,13 @@ class EquipmentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $path = __DIR__.'/data/Equipment.json';
+        $content = file_get_contents($path);
+        $content_decode = json_decode($content, true);
 
-        $manager->flush();
+        foreach ($content_decode as $equipment) {
+            EquipmentFactory::createOne($equipment);
+        }
+
     }
 }
