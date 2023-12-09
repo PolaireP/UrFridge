55430b598bf023184ac10bcb0b7beafd2475f4e7
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2023-12-09 16:02:45 +0100

    Ajout fixture pour les catégories à partir du fichier /data/Category.json

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,6 +11,12 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $data = json_decode(file_get_contents(__DIR__.'/data/Category.json'), true);
 
+        foreach ($data as $category) {
+            CategoryFactory::createOne($category);
+        }
+
+        $manager->flush();
     }
 }
