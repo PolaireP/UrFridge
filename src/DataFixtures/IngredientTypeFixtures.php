8fb8e390588f889ab5c16b8a298c274ae0354fdc
William Peoc'h <peoch.william44@gmail.com>
2023-12-10 11:49:16 +0100

    Création du générateur de nom des types d'ingrédients

--- src/DataFixtures/IngredientTypeFixtures.php
+++ src/DataFixtures/IngredientTypeFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\IngredientTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,5 +11,11 @@ class IngredientTypeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $content = file_get_contents(__DIR__.'/data/IngredientType.json');
+        $decode_content = json_decode($content, true);
+
+        foreach ($decode_content as $ingredientType) {
+            IngredientTypeFactory::createOne($ingredientType);
+        }
     }
 }
