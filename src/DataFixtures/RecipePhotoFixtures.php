6346022f6de86e61d81089ae9fab8f91374005aa
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2023-12-19 21:30:55 +0100

    Suppression des commentaires inutiles

--- src/DataFixtures/RecipePhotoFixtures.php
+++ src/DataFixtures/RecipePhotoFixtures.php
@@ -11,14 +11,11 @@ class RecipePhotoFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
         $data = json_decode(file_get_contents(__DIR__.'/data/RecipePhoto.json'), true);
         // Itération du json pour charger le contenu des images dans recipePhoto
         foreach($data as $key => $element) {
             $data[$key]['recipePhoto'] = file_get_contents(__DIR__."/data/".$element['recipePhoto']);
-            ## dump($data[$key]['recipePhoto']);
         }
 
         $factory = RecipePhotoFactory::createSequence($data);
