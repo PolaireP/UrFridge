0fe1d49cb29d25afed534650e7ca85a69cd1d745
dura0074 <raphael.durand1@etudiant.univ-reims.fr>
2023-12-22 18:26:55 +0100

    Début js accueil

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+const filtersArea = document.getElementsByClassName("filters-with-result-statistics-area")[0];
+const resultsNumber = document.getElementsByClassName("results-number")[0];
+
+resultsNumber.addEventListener("click", function () {
+    
+});
