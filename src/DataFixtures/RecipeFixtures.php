f7df160a6b0393f624415efe2a2fcf077e26945b
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2023-12-23 16:18:23 +0100

    Mise en place de la création des fixtures de Recipe

--- src/DataFixtures/RecipeFixtures.php
+++ src/DataFixtures/RecipeFixtures.php
@@ -2,21 +2,31 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\RecipePhoto;
 use App\Factory\RecipeFactory;
 use App\Factory\RecipePhotoFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class RecipeFixtures extends Fixture
+class RecipeFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        // Chargement des données
+        $data = json_decode(file_get_contents(__DIR__.'/data/Recipe.json'), true);
+        $photoId = RecipePhotoFactory::first()->getId();
 
+        // Opération sur les données (recherche des RecipePhoto et ajout )
+        foreach($data as $key => $element) {
+            $data[$key]['recipePhoto'] = RecipePhotoFactory::findBy(['id' => $photoId + $data[$key]['recipePhoto']])[0];
+        }
+
+        // Création de la séquence
+        $factory = RecipeFactory::createSequence($data);
     }
 
-    public function getDepencies() {
+    public function getDependencies() : array {
         return [
             RecipePhotoFixtures::class
         ];
