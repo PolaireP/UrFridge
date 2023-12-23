58a685282139cea2ae371a275919454c17c3d08f
William Peoc'h <william.peoch@etudiant.univ-reims.fr>
2023-12-23 16:30:34 +0100

    Implémentation de DependentFixtureInterface

--- src/DataFixtures/PersonFixtures.php
+++ src/DataFixtures/PersonFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use App\Factory\InventoryFactory;
 use App\Factory\PersonFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PersonFixtures extends Fixture
+class PersonFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
