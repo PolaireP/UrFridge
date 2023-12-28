d644f6c2b5c7db1bd8541b90548a190fec1a73cb
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2023-12-28 17:47:58 +0100

    Modification de StepFixtures.php

--- src/DataFixtures/StepFixtures.php
+++ src/DataFixtures/StepFixtures.php
@@ -2,17 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\RecipeFactory;
+use App\Factory\StepFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class StepFixtures extends Fixture
+class StepFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Chargement des données
+        $data = json_decode(file_get_contents(__DIR__.'/data/Step.json'), true);
+        $recipeId = RecipeFactory::first()->getId();
 
-        $manager->flush();
+        // Opération sur les données (recherche des RecipePhoto et ajout )
+        foreach($data as $key => $element) {
+            $data[$key]['recipe'] = RecipeFactory::findBy(['id' => $recipeId + $data[$key]['recipe']])[0];
+        }
+
+        $factory = StepFactory::createSequence($data);
+    }
+
+    public function getDependencies()
+    {
+        return [
+            RecipeFixtures::class
+        ];
+
     }
 }
