d2f45b7f4cfc9a103f5c1c8b98eeb576fb1a57f1
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2024-01-03 14:58:52 +0100

    Création d'EquipmentPhoto et mise à jour d'equipment

---
+++ src/Entity/EquipmentPhoto.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentPhotoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipmentPhotoRepository::class)]
+class EquipmentPhoto
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $equipmentPhoto = null;
+
+    #[ORM\OneToMany(mappedBy: 'equipmentPhoto', targetEntity: Equipment::class)]
+    private Collection $equipment;
+
+    public function __construct()
+    {
+        $this->equipment = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEquipmentPhoto()
+    {
+        return $this->equipmentPhoto;
+    }
+
+    public function setEquipmentPhoto($equipmentPhoto): static
+    {
+        $this->equipmentPhoto = $equipmentPhoto;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Equipment>
+     */
+    public function getEquipment(): Collection
+    {
+        return $this->equipment;
+    }
+
+    public function addEquipment(Equipment $equipment): static
+    {
+        if (!$this->equipment->contains($equipment)) {
+            $this->equipment->add($equipment);
+            $equipment->setEquipmentPhoto($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEquipment(Equipment $equipment): static
+    {
+        if ($this->equipment->removeElement($equipment)) {
+            // set the owning side to null (unless already changed)
+            if ($equipment->getEquipmentPhoto() === $this) {
+                $equipment->setEquipmentPhoto(null);
+            }
+        }
+
+        return $this;
+    }
+}
