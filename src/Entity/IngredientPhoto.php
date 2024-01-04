7140e8353d3abaaefbab100382b47bf1149f75fe
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-04 14:55:09 +0100

    création entité IngredientPhoto

---
+++ src/Entity/IngredientPhoto.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientPhotoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientPhotoRepository::class)]
+class IngredientPhoto
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $ingredient_photo = null;
+
+    #[ORM\OneToMany(mappedBy: 'ingredient_photo', targetEntity: Ingredient::class)]
+    private Collection $ingredients;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIngredientPhoto()
+    {
+        return $this->ingredient_photo;
+    }
+
+    public function setIngredientPhoto($ingredient_photo): static
+    {
+        $this->ingredient_photo = $ingredient_photo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ingredient>
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(Ingredient $ingredient): static
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients->add($ingredient);
+            $ingredient->setIngredientPhoto($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(Ingredient $ingredient): static
+    {
+        if ($this->ingredients->removeElement($ingredient)) {
+            // set the owning side to null (unless already changed)
+            if ($ingredient->getIngredientPhoto() === $this) {
+                $ingredient->setIngredientPhoto(null);
+            }
+        }
+
+        return $this;
+    }
+}
