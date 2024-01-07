9c4ca41e31b1b4816ed7232d00ddfb4fdd9e91ea
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2024-01-07 16:55:36 +0100

    Création des factory nécessaires à Recipe.php

---
+++ src/Factory/FridgeQuantityFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\FridgeQuantity;
+use App\Repository\FridgeQuantityRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<FridgeQuantity>
+ *
+ * @method        FridgeQuantity|Proxy create(array|callable $attributes = [])
+ * @method static FridgeQuantity|Proxy createOne(array $attributes = [])
+ * @method static FridgeQuantity|Proxy find(object|array|mixed $criteria)
+ * @method static FridgeQuantity|Proxy findOrCreate(array $attributes)
+ * @method static FridgeQuantity|Proxy first(string $sortedField = 'id')
+ * @method static FridgeQuantity|Proxy last(string $sortedField = 'id')
+ * @method static FridgeQuantity|Proxy random(array $attributes = [])
+ * @method static FridgeQuantity|Proxy randomOrCreate(array $attributes = [])
+ * @method static FridgeQuantityRepository|RepositoryProxy repository()
+ * @method static FridgeQuantity[]|Proxy[] all()
+ * @method static FridgeQuantity[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static FridgeQuantity[]|Proxy[] createSequence(iterable|callable $sequence)
+ * @method static FridgeQuantity[]|Proxy[] findBy(array $attributes)
+ * @method static FridgeQuantity[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static FridgeQuantity[]|Proxy[] randomSet(int $number, array $attributes = [])
+ */
+final class FridgeQuantityFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'ingredient' => null, // TODO add App\Entity\ingredient type manually
+            'inventory' => null, // TODO add App\Entity\inventory type manually
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(FridgeQuantity $fridgeQuantity): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return FridgeQuantity::class;
+    }
+}
