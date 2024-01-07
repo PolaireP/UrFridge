96ec32bfddac24ee4ebb04389d6927be1772de2a
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2024-01-07 12:25:15 +0100

    Création de PersonFixtures.php et ajout de changements dans security.yaml

--- src/DataFixtures/PersonFixtures.php
+++ src/DataFixtures/PersonFixtures.php
@@ -2,27 +2,37 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\InventoryFactory;
 use App\Factory\PersonFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PersonFixtures extends Fixture implements DependentFixtureInterface
+class PersonFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        PersonFactory::createMany(20, function () {
-            return [
-                'inventory' => InventoryFactory::createOne(),
-            ];
-        });
-    }
+        // $product = new Product();
+        // $manager->persist($product);
 
-    public function getDependencies()
-    {
-        return [
-            InventoryFixtures::class,
-        ];
+        PersonFactory::createSequence(
+            [
+                'email' => 'root@example.com',
+                'firstname' => 'Tony',
+                'lastname' => 'Stark',
+                'roles' => ['ROLE_ADMIN'],
+            ]
+        );
+
+        PersonFactory::createSequence([
+            [
+                'email' => 'user@example.com',
+                'firstname' => 'Peter',
+                'lastname' => 'Parker',
+                'roles' => ['ROLE_USER'],
+            ],
+        ]);
+
+        PersonFactory::createMany(10);
+
+        $manager->flush();
     }
 }
