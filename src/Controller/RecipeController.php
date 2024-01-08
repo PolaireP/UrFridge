00482fd81a37739903477712fa18dae21770c0bb
William Peoc'h <william.peoch@etudiant.univ-reims.fr>
2024-01-08 21:50:44 +0100

    Création de la route index pour la recherche de recettes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,17 +4,27 @@ namespace App\Controller;
 
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
     #[Route('/recipe', name: 'app_recipe')]
-    public function index(RecipeRepository $repository): Response
+    public function index(RecipeRepository $repository, Request $request): Response
     {
+        $searchString = $request->query->get('search-recipes', '');
+        $recipes = $repository->search($searchString);
+
+        $images = [];
+        foreach ($recipes as $key => $recipe) {
+            $images[$recipe->getRecipeId()] = base64_encode(stream_get_contents($recipe->getRecipePhoto()->getRecipePhoto()));
+        }
 
         return $this->render('pages/recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+            'recipes' => $recipes,
+            'images' => $images,
+            'searchString' => $searchString,
         ]);
     }
 }
