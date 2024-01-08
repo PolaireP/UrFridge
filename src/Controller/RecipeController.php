1c582424e051b14c33a044ec759f696f4f405921
William Peoc'h <william.peoch@etudiant.univ-reims.fr>
2024-01-08 22:05:28 +0100

    Ajout du listage des allergènes dans les filtres allergènes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Allergen;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,20 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recipe', name: 'app_recipe')]
-    public function index(RecipeRepository $repository, Request $request): Response
+    public function index(RecipeRepository $repository, Request $request, EntityManagerInterface $manager): Response
     {
+        // Recherche des recettes
         $searchString = $request->query->get('search-recipes', '');
         $recipes = $repository->search($searchString);
 
+        // Liste des images des recettes
         $images = [];
         foreach ($recipes as $key => $recipe) {
             $images[$recipe->getRecipeId()] = base64_encode(stream_get_contents($recipe->getRecipePhoto()->getRecipePhoto()));
         }
 
+        // Listage de tous les allergènes
+        $allergens = $manager->getRepository(Allergen::class)->findAll();
+
         return $this->render('pages/recipe/index.html.twig', [
             'recipes' => $recipes,
             'images' => $images,
             'searchString' => $searchString,
+            'allergens' => $allergens,
         ]);
     }
 }
