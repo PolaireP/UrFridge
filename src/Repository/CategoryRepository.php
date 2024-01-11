155d87c1ad3632fdf9bdc7ceeacf4dbfa7848bf7
“dura0074” <raphael.durand1@etudiant.univ-reims.fr>
2024-01-12 00:43:39 +0100

    Ajout de méthode dans les repositorys pour récupérer les entités

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -21,28 +21,15 @@ class CategoryRepository extends ServiceEntityRepository
         parent::__construct($registry, Category::class);
     }
 
-    //    /**
-    //     * @return Category[] Returns an array of Category objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function search(string $text = ''): array
+    {
+        $qb = $this->createQueryBuilder('ca')
+            ->where('ca.categoryName LIKE :text')
+            ->setParameter('text', '%'.$text.'%')
+            ->orderBy('ca.categoryName', 'ASC');
 
-    //    public function findOneBySomeField($value): ?Category
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        $query = $qb->getQuery();
+
+        return $query->execute();
+    }
 }
