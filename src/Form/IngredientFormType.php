7c64292755d57f57d9f9c44d64b7688b5517abb4
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-11 17:05:27 +0100

    refractor rename IngredientForm -> IngredientFormType

--- src/Form/IngredientFormType.php
+++ src/Form/IngredientFormType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class IngredientForm extends AbstractType
+class IngredientFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
