d8b61c11075f7b2aa3797c4db3e4a411fcc7972a
poir0049 <romain.poirrier@etudiant.univ-reims.fr>
2024-01-11 23:14:09 +0100

    Développement de la suppression de profile

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -6,17 +6,23 @@ use App\Entity\Person;
 use App\Entity\PersonPhoto;
 use App\Form\PersonPhotoType;
 use App\Form\PersonType;
+use App\Repository\CommentaryRepository;
+use App\Repository\CommentRepository;
 use App\Repository\PersonPhotoRepository;
 use App\Repository\PersonRepository;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -58,7 +64,7 @@ class PersonController extends AbstractController
 
 
         //dump(base64_encode(stream_get_contents($userInfos[0]['person']->getAvatar()->getPersonPhoto())));
-        return $this->render('person/index.html.twig', [
+        return $this->render('pages/person/index.html.twig', [
             'controller_name' => 'PersonController',
             'user' => $repository[0]['person'],
             'avatar' => $avatar,
@@ -69,7 +75,7 @@ class PersonController extends AbstractController
     }
 
     #[Route('/profil/{id}/update', requirements: ['id' => '\d+'])]
-    public function update(Person $person, PersonPhoto $personPhoto = null, PersonPhotoRepository $rep, Request $request, EntityManagerInterface $manager): Response {
+    public function update(Person $person = null, PersonPhoto $personPhoto = null, PersonPhotoRepository $rep, Request $request, EntityManagerInterface $manager): Response {
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -86,7 +92,69 @@ class PersonController extends AbstractController
         }
 
 
-        $form = $this->createForm(PersonType::class, $person, ['attr' => [ 'class' => 'edit-profile-informations-form' ]]);
+        $form = $this->createForm(PersonType::class, $person, ['attr' => [ 'class' => 'edit-profile-informations-form' ]])
+            ->add('firstname', options: [
+                'required' => true,
+                'label' => false,
+                'row_attr' => ['id' => 'firstname'],
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                    'class' => 'profile-information-input',
+                    'autocomplete' => 'given-name',
+                    'id' => 'firstname',
+                ],
+            ])
+            ->add('lastname', options: [
+                'required' => true,
+                'label' => false,
+                'row_attr' => ['id' => 'lastname'],
+                'attr' => [
+                    'placeholder' => 'Nom',
+                    'class' => 'profile-information-input',
+                    'autocomplete' => 'family-name',
+                    'id' => 'lastname',
+                ],
+            ])
+            ->add('email', options: [
+                'required' => true,
+                'label' => false,
+                'row_attr' => ['id' => 'email'],
+                'attr' => [
+                    'placeholder' => 'Email',
+                    'class' => 'profile-information-input',
+                    'autocomplete' => 'email',
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Merci d'entrer un mot de passe",
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'minMessage' => 'Votre mot de passe doit faire {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options' => [
+                    'attr' => ['placeholder' => 'Mot de passe', 'class' => 'profile-information-input'],
+                    'label' => false,
+                    'row_attr' => ['id' => 'password'],
+                ],
+                'second_options' => [
+                    'attr' => ['placeholder' => 'Confirmer le mot de passe', 'class' => 'profile-information-input'],
+                    'label' => false,
+                    'row_attr' => ['id' => 'confirm-password'],
+                ],
+            ]);
+
         //$imageForm = $this->createForm(PersonPhotoType::class,$personPhoto, ['attr' => ['class' => 'profile-picture-input']]);
 
         $form->handleRequest($request);
@@ -99,10 +167,59 @@ class PersonController extends AbstractController
         }
 
 
-        return $this->render('person/update.html.twig', [
+        return $this->render('pages/person/update.html.twig', [
             'user' => $person,
             'updateForm' => $form,
             'avatar' => $avatar,
+        ]);
+    }
+
+    #[Route('/profil/{id}/delete', requirements: ['id' => '\d+'])]
+    public function delete(Person $person = null, SessionInterface $session, RecipeRepository $rep, CommentRepository $cmtRep, CommentaryRepository $cmtrRep, EntityManagerInterface $manager, Request $req): Response {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+
+        if ($this->getUser()->getId() != $person->getId()) {
+            return $this->redirectToRoute('app_person_delete', ['id' => $this->getUser()->getId()]);
+        }
+
+        $deleteForm = $this->createForm(PersonType::class, $person)
+            ->add('supprimer', SubmitType::class)
+            ->add('annuler', SubmitType::class);
+
+        $deleteForm->handleRequest($req);
+
+        if ($deleteForm->isSubmitted() && $deleteForm->isValid()) {
+            if ($deleteForm->getClickedButton() === $deleteForm->get('supprimer')) {
+
+                $recipesToDelete = $rep->findBy(['author' => $person->getId()]);
+                foreach ($recipesToDelete as $elem) {
+                    $elem->setAuthor(null);
+                }
+
+                $commentsToDelete = $cmtRep->findBy(['writer' => $person->getId()]);
+                foreach ($commentsToDelete as $elem) {
+                    $commentary = $cmtrRep->findOneBy(['id' => $elem->getCommentary()]);
+                    $elem->setCommentary(null);
+                    $elem->setWriter(null);
+                    $elem->setRecipe(null);
+                    $manager->remove($commentary);
+                    $manager->remove($elem);
+                }
+
+                $manager->remove($person);
+                $req->getSession()->invalidate();
+                $this->container->get('security.token_storage')->setToken(null);
+                $manager->flush();
+
+                return $this->redirectToRoute('app_home');
+            } else {
+                return $this->redirectToRoute('app_profil');
+            }
+        }
+
+        return $this->render('pages/person/delete.html.twig', [
+            'deleteForm' => $deleteForm,
         ]);
     }
 }
