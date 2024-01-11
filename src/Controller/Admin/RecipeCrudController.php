bc9f4c2ad10fcf93d71d76e4ac52012694ad8313
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-11 14:54:25 +0100

    configuration du crud de Recipe: ajout crudController

---
+++ src/Controller/Admin/RecipeCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Recipe;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class RecipeCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Recipe::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            AssociationField::new('recipePhoto', 'idPhoto')
+                ->setFormTypeOptions([
+                    'class' => 'App\Entity\RecipePhoto',
+                    'choice_label' => 'id',
+                ])
+                ->setCrudController(RecipePhotoCrudController::class),
+            TextField::new('recipeName'),
+            TextEditorField::new('recipeDescription'),
+            DateField::new('recipePubDate'),
+            BooleanField::new('verified'),
+        ];
+    }
+}
