21503a67782cfb14992d36446a437ea3deaf9827
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-12 02:34:47 +0100

    Suppression gestion des mdp

--- src/Controller/Admin/PersonCrudController.php
+++ src/Controller/Admin/PersonCrudController.php
@@ -3,40 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Person;
-use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class PersonCrudController extends AbstractCrudController
 {
-    private $passwordHasher;
-
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
-    {
-        $this->passwordHasher = $passwordHasher;
-    }
-
-    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        $request = $this->getContext()->getRequest();
-
-        $plainPassword = $request->request->get('Person')['password'];
-
-        if ($plainPassword !== null && $plainPassword !== '') {
-            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $plainPassword);
-            $entityInstance->setPassword($hashedPassword);
-        }
-
-        parent::updateEntity($entityManager, $entityInstance);
-    }
-
     public static function getEntityFqcn(): string
     {
         return Person::class;
@@ -44,14 +18,7 @@ class PersonCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-
-
         $rolesField = ArrayField::new('roles');
-
-        $passwordField = TextField::new('password', PasswordType::class)
-            ->onlyOnForms()
-            ->setRequired(false)
-            ->setEmptyData('');
 
         $idField = Field::new('id')->hideOnForm();
 
@@ -64,7 +31,7 @@ class PersonCrudController extends AbstractCrudController
                     'class' => 'App\Entity\Inventory',
                     'choice_label' => 'id',
                 ])
-                ->setCrudController(RecipePhotoCrudController::class),
-            $idField, $rolesField, $passwordField];
+                ->setCrudController(InventoryCrudController::class),
+            $idField, $rolesField];
     }
 }
