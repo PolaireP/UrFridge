d9bf508ecd28a66c71809380f7da2d82c20719a7
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-12 01:09:31 +0100

    Configuration champs Allergen

--- src/Controller/Admin/AllergenCrudController.php
+++ src/Controller/Admin/AllergenCrudController.php
@@ -19,9 +19,9 @@ class AllergenCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('allergenName'),
+            TextField::new('allergenDescription'),
         ];
     }
 
