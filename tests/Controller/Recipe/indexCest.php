922de10a4ebcc15c9318c6d1e43d900a8215a48f
char0264 <lucas.charmettan@etudiant.univ-reims.fr>
2024-01-12 03:01:15 +0100

    Création d' un  Cest dédié à l'action index du recipeController

---
+++ tests/Controller/Recipe/indexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Recipe;
+
+use App\\Tests\Support\ControllerTester;
+
+class indexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
